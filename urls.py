eb1138faa5765bd26a817a2c450ed5b82c7583e7
PunamDange <punamdange2255@gmail.com>
2025-06-08 13:55:44 +0530

    Delete urls.py

--- urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import RegisterUserView, LoginView, UserListView
-
-urlpatterns = [
-    path('register/', RegisterUserView.as_view(), name='register'),
-    path('login/', LoginView.as_view(), name='login'),
-    path('users/', UserListView.as_view(), name='user-list'),
-]
